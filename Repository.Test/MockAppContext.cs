739aceb9b6beb6136987ec506e359d2522563a49
Renan Augusto <renan.augusto18@hotmail.com>
2015-11-26 00:25:42 -0200

    Repository.Test Project

---
+++ Repository.Test/MockAppContext.cs
@@ -0,0 +1,87 @@
+﻿using Domain.Models;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace Repository.Test
+{
+    public class MockAppContext : DbContext
+    {
+        public MockAppContext() 
+            : base("Name=MockAppContext") 
+        {
+
+        }
+        public MockAppContext(bool enableLazyLoading, bool enableProxyCreation) 
+            : base("Name=MockAppContext") 
+        {
+            Configuration.ProxyCreationEnabled = enableProxyCreation;
+            Configuration.LazyLoadingEnabled = enableLazyLoading;
+        }
+        public MockAppContext(DbConnection connection) 
+            : base(connection, true) 
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Suppress code first model migration check           
+            Database.SetInitializer<MockAppContext>(new AlwaysCreateInitializer());
+
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public void Seed(MockAppContext Context)
+        {
+            var usuarios = new List<Usuario>()
+            {
+                new Usuario ( "Renan", "renan", "renan.augusto18@gmail.com", "123456" ),
+                new Usuario ( "Renan2", "renan2", "renan.augusto218@gmail.com", "123456" ),
+                new Usuario ( "Renan3", "renan3", "renan.augusto318@gmail.com", "123456" ),
+                new Usuario ( "Renan4", "renan4", "renan.augusto418@gmail.com", "123456" ),
+            }.AsQueryable();
+
+            Context.Usuarios.AddRange(usuarios);
+            Context.SaveChanges();
+        }
+
+        public class DropCreateIfChangeInitializer : DropCreateDatabaseIfModelChanges<MockAppContext>
+        {
+            protected override void Seed(MockAppContext context)
+            {
+                context.Seed(context);
+                base.Seed(context);
+            }
+        }
+
+        public class CreateInitializer : CreateDatabaseIfNotExists<MockAppContext>
+        {
+            protected override void Seed(MockAppContext context)
+            {
+                context.Seed(context);
+                base.Seed(context);
+            }
+        }
+
+        public class AlwaysCreateInitializer : DropCreateDatabaseAlways<MockAppContext>
+        {
+            protected override void Seed(MockAppContext context)
+            {
+                context.Seed(context);
+                base.Seed(context);
+            }
+        }
+
+
+    }
+}
---
+++ Repository.Test/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository;
+using Moq;
+using Repository.Usuarios;
+using Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using Domain;
+using System.Data.Common;
+
+namespace Repository.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        DbConnection connection;
+        MockAppContext databaseContext;
+        UsuarioRepository objRepo;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            connection = Effort.DbConnectionFactory.CreateTransient();
+            databaseContext = new MockAppContext(connection);
+            objRepo = new UsuarioRepository(databaseContext);
+
+        }
+
+        [TestMethod]
+        public void BuscaPeloLogin_Test()
+        {
+            // Try finding a product by id
+            var usuario = objRepo.BuscaPeloLogin("renan");
+
+            Assert.AreEqual("123456", usuario.Senha);
+            Assert.AreEqual("renan.augusto18@gmail.com", usuario.Email);
+
+        }
+    }
+}
